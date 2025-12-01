39f6d3a8da9fd7fcb26bc54d40648172abf7f6f8
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-12-01 16:07:47 +0530

    update 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-// next.config.mjs
-export default {
-  output: 'export',
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // images: { unoptimized: true }, // uncomment if using <Image> with export
+};
+
+export default nextConfig;
