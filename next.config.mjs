7b7c3a0034456f7883c353d2968f67dbd2331fa7
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2025-12-17 16:07:24 +0530

    17/12/2025 add admin chanels

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   // reactCompiler: true,
+
+   async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://adminayatiworks.vercel.app",
+      },
+      {
+        source: "/admin/:path*",
+        destination: "https://adminayatiworks.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
