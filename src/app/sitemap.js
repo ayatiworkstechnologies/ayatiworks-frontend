0a3fb9de3d4719446e4745a7d76a7b6d218ac768
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2026-01-23 13:29:24 +0530

    23/01/2026 sitemap update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -5,60 +5,66 @@ export const revalidate = false;
 const baseUrl = "https://ayatiworks.com";
 
 const routes = [
-    "/", // Home
-    "/about-us",
-    "/team",
-    "/careers",
+  "/", // Home
+  "/about-us",
+  "/team",
+  "/careers",
 
-    // Digital Marketing Services
-    "/digital-marketing-services",
-    "/digital-marketing-services/seo",
-    "/digital-marketing-services/social-media-marketing",
-    "/digital-marketing-services/email-marketing",
-    "/digital-marketing-services/instagram-marketing",
-    "/digital-marketing-services/affiliate-marketing",
-    "/digital-marketing-services/programmatic-marketing",
-    "/digital-marketing-services/video-marketing",
+  // Digital Marketing Services
+  "/digital-marketing-services",
+  "/digital-marketing-services/seo",
+  "/digital-marketing-services/social-media-marketing",
+  "/digital-marketing-services/email-marketing",
+  "/digital-marketing-services/instagram-marketing",
+  "/digital-marketing-services/affiliate-marketing",
+  "/digital-marketing-services/programmatic-marketing",
+  "/digital-marketing-services/video-marketing",
 
-    // Content-as-a-Service
-    "/content-as-a-service",
-    "/content-as-a-service/branding-service",
-    "/content-as-a-service/video-creation",
-    "/content-as-a-service/multilingual-marketing",
+  // Content-as-a-Service
+  "/content-as-a-service",
+  "/content-as-a-service/branding-service",
+  "/content-as-a-service/video-creation",
+  "/content-as-a-service/multilingual-marketing",
 
-    // Digital PR Service
-    "/digital-pr",
-    "/digital-pr/digital-pr-services",
-    "/digital-pr/influencer-marketing",
-    "/digital-pr/online-reputation-media-outreach",
+  // Digital PR Service
+  "/digital-pr",
+  "/digital-pr/digital-pr-services",
+  "/digital-pr/influencer-marketing",
+  "/digital-pr/online-reputation-media-outreach",
 
-    // Web & E-commerce
-    "/web-ecommerce",
-    "/web-ecommerce/ux-ui-design",
-    "/web-ecommerce/web-development",
-    "/web-ecommerce/web-maintenance",
-    "/web-ecommerce/shopify-development",
-    "/web-ecommerce/ecommerce-solutions",
+  // Web & E-commerce
+  "/web-ecommerce",
+  "/web-ecommerce/ux-ui-design",
+  "/web-ecommerce/web-development",
+  "/web-ecommerce/web-maintenance",
+  "/web-ecommerce/shopify-development",
+  "/web-ecommerce/ecommerce-solutions",
 
-    // Insights
-    "/blogs",
-    "/awards",
-    "/case-study",
-    "/case-study/Reposition-Nippo-and-Brand-Awareness-Campaign",
-    "/case-study/Jeep-India-Independence-Day-Merchandise-Sales-Campaign",
-    "/case-study/how-Ayatiworks-propelled-Volvos-digital-presence-by-250-percentage-increase-in-impressions",
+  // Insights
+  "/blogs",
+  "/awards",
+  "/case-study",
+  "/case-study/Reposition-Nippo-and-Brand-Awareness-Campaign",
+  "/case-study/Jeep-India-Independence-Day-Merchandise-Sales-Campaign",
+  "/case-study/how-Ayatiworks-propelled-Volvos-digital-presence-by-250-percentage-increase-in-impressions",
 
-    // Contact
-    "/contact-us",
+  // Contact
+  "/contact-us",
 
-    //blogs List 
+  //blogs List 
 
-    "/blogs/seo/5-must-know-aeo-strategies-for-2025",
-    "/blogs/seo/how-your-tech-startup-can-use-answer-engine-optimization-aeo-to-attract-engage-and-convert-smarter-audiences",
-    "/blogs/seo/implementing-aeo-in-your-content-strategy",
-    "/blogs/digital-marketing-services/chennai-digital-marketing-services",
-    "/blogs/digital-marketing-services/benefits-of-hiring-a-chennai-based-digital-marketing-agency",
-  ];
+  "/blogs/seo/5-must-know-aeo-strategies-for-2025",
+  "/blogs/seo/how-your-tech-startup-can-use-answer-engine-optimization-aeo-to-attract-engage-and-convert-smarter-audiences",
+  "/blogs/seo/implementing-aeo-in-your-content-strategy",
+  "/blogs/digital-marketing-services/chennai-digital-marketing-services",
+  "/blogs/digital-marketing-services/benefits-of-hiring-a-chennai-based-digital-marketing-agency",
+  "/blogs/digital-marketing-services/how-to-evaluate-a-digital-marketing-agency-in-chennai/",
+  "/blogs/digital-marketing-services/top-10-digital-marketing-agencies-in-chennai/",
+  "/blogs/digital-marketing-services/digital-marketing-integrated-growth-framework/",
+  "/blogs/digital-marketing-services/business-decision-guide-choosing-prioritising-scaling-services/",
+  "/blogs/seo/seo-vs-aeo-vs-geo-vs-ai-seo-why-seo-is-still-the-foundation/",
+  "/blogs/digital-marketing-services/digital-marketing-services-drive-revenue-funnel-alignment/",
+];
 
 export default function sitemap() {
   const now = new Date();
@@ -70,4 +76,4 @@ export default function sitemap() {
   }));
 }
 
-  
+
