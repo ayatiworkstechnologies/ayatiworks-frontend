dac107659e53aa8bd03403acde437afaf8419027
rubankumarsankar <rubankumarsankar@users.noreply.github.com>
2026-02-20 15:36:52 +0530

    20/2/2026 update 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 5000,
+  generateIndexSitemap: false, // Output one comprehensive sitemap.xml
   exclude: ["/admin/*", "/api/*"],
   robotsTxtOptions: {
     policies: [
