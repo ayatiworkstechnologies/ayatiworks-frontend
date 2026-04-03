5ab01cf106f5487b981b1b6469bf10bc6f49b90e
rubankumarsankar <srirubankumar@gmail.com>
2026-04-03 11:06:39 +0530

    3/4/2026 update correcton speed 1

--- src/app/components/Insights/InsightsPages.jsx
+++ src/app/components/Insights/InsightsPages.jsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+
 "use client";
 
 import Link from "next/link";
 import { motion } from "framer-motion";
+import Image from "next/image";
 import {
 
   ArrowRight,
