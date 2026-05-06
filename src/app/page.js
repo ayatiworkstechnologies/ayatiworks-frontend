7e6060219cc7a16e33d05ceef39e71e9c8813f8e
rubankumarsankar <srirubankumar@gmail.com>
2026-05-06 18:53:30 +0530

    6/5/2026 2

--- src/app/page.js
+++ src/app/page.js
@@ -122,7 +122,6 @@ export default function HomePage() {
           __html: JSON.stringify(localBusinessJsonLd),
         }}
       />
-      <>12343</>
       <HeroSection />
       <PromoHero />
       <AyatiAboutSection />
